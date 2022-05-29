aceb6ac181648b4897e7037668a872147a28897c
jseo123 <jose123135@gmail.com>
2022-05-30 01:43:34 +0200

    Created auth context

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,19 @@
 import "./App.css";
 import Main from "./Main";
 import Login from "./Login";
+import { AuthProvider } from "./context/authContext";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/log" element={<Login />} />
-        </Routes>
+        <AuthProvider>
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/log" element={<Login />} />
+          </Routes>
+        </AuthProvider>
       </div>
     </BrowserRouter>
   );
--- my-app/src/Login.js
+++ my-app/src/Login.js
@@ -1,21 +1,23 @@
 import "./App.css";
 import "./Login.css";
+import { useAuth } from "./context/authContext";
 
 const Login = () => {
+  const { user } = useAuth();
+  console.log(user);
   return (
     <div className="formContainer">
       <div className="imgHolder">
-        {" "}
         <img src="/images/giphyLogo.png" alt="Page Logo"></img>
       </div>
       <form className="logForm">
         <div className="formHolder">
-          <label for="user"> Insert your username or email</label>
+          <label htmlFor="user"> Insert your username or email</label>
           <input name="user" type="text"></input>
         </div>
 
         <div className="formHolder">
-          <label for="password">Please introduce your password</label>
+          <label htmlFor="password">Please introduce your password</label>
           <input name="password" type="password"></input>
         </div>
 
